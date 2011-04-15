6edc1266a16f08964f9b512fe58c474cafebd9de
DrLecter <DrLecter@users.noreply.github.com>
2011-04-15 23:16:57 +0000

    Populating the unstable branch

--- L2J_CommunityServer/dist/startCommunityServer.bat
+++
@@ -1,6 +0,0 @@
-@echo off
-title Community Server Console
-echo Starting L2J Community Server.
-echo.
-java -Xms128m -Xmx128m -cp ./../libs/*;l2jcommunity.jar com.l2jserver.communityserver.L2CommunityServer
-pause
--- L2J_CommunityServer/dist/startCommunityServer.sh
+++
@@ -1,2 +0,0 @@
-#!/bin/sh
-java -Xms128m -Xmx128m -cp ./../libs/*:l2jcommunity.jar com.l2jserver.communityserver.L2CommunityServer > log/stdout.log 2>&1
