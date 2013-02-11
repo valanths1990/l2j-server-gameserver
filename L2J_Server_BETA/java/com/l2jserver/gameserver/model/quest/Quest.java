8b1455d403558098342ec2923f8220882dd46924
Zoey76 <zoey_76@msn.com>
2013-02-11 23:09:52 +0000

    BETA: Restoring some methods for Jython scripts.
    Reported by: oscard

--- L2J_Server_BETA/java/com/l2jserver/gameserver/model/quest/Quest.java
+++ L2J_Server_BETA/java/com/l2jserver/gameserver/model/quest/Quest.java
@@ -1882,6 +1882,32 @@ public class Quest extends ManagedScript
 		}
 	}
 	
+	// TODO: Remove after all Jython scripts are replaced with Java versions.
+	public void addStartNpc(int npcId)
+	{
+		addEventId(QuestEventType.QUEST_START, npcId);
+	}
+	
+	public void addFirstTalkId(int npcId)
+	{
+		addEventId(QuestEventType.ON_FIRST_TALK, npcId);
+	}
+	
+	public void addTalkId(int npcId)
+	{
+		addEventId(QuestEventType.ON_TALK, npcId);
+	}
+	
+	public void addKillId(int killId)
+	{
+		addEventId(QuestEventType.ON_KILL, killId);
+	}
+	
+	public void addAttackId(int npcId)
+	{
+		addEventId(QuestEventType.ON_ATTACK, npcId);
+	}
+	
 	/**
 	 * Add the quest to the NPC's startQuest
 	 * @param npcIds
