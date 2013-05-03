6aebbd156b1334808e82010307090b03e56ddcfd
nBd <nBdde82@gmail.com>
2013-05-03 13:51:49 +0000

    BETA: Fixing SelfEffects, they should be added to CharEffectList

--- L2J_Server_BETA/java/com/l2jserver/gameserver/model/skills/L2Skill.java
+++ L2J_Server_BETA/java/com/l2jserver/gameserver/model/skills/L2Skill.java
@@ -1561,12 +1561,9 @@ public abstract class L2Skill implements IChanceSkillTrigger
 			}
 		}
 		
-		if (effects.isEmpty())
-		{
-			return EMPTY_EFFECT_SET;
-		}
-		
-		return effects.toArray(new L2Effect[effects.size()]);
+		final L2Effect[] list = effects.toArray(new L2Effect[effects.size()]);
+		effected.getEffectList().add(list);
+		return effects.isEmpty() ? EMPTY_EFFECT_SET : list;
 	}
 	
 	public final L2Effect[] getEffectsSelf(L2Character effector)
@@ -1592,11 +1589,9 @@ public abstract class L2Skill implements IChanceSkillTrigger
 			}
 		}
 		
-		if (effects.isEmpty())
-		{
-			return EMPTY_EFFECT_SET;
-		}
-		return effects.toArray(new L2Effect[effects.size()]);
+		final L2Effect[] list = effects.toArray(new L2Effect[effects.size()]);
+		effector.getEffectList().add(list);
+		return effects.isEmpty() ? EMPTY_EFFECT_SET : list;
 	}
 	
 	public final L2Effect[] getPassiveEffects(L2Character effector)
