1c130bcf469776af51c74b9f600808f48c0d0a2c
Nos <NosBit@users.noreply.github.com>
2013-09-24 21:06:44 +0000

    BETA: `RequestGiveNickName` packet fixes.     * Fixed possible `PatternSyntaxException`         * Reported by: lucan         * Patch by: jurchiks     * Changed custom messages to retail like messages.
    Reviewed by: UnAfraid, Zoey76

--- L2J_Server_BETA/java/com/l2jserver/gameserver/network/clientpackets/RequestGiveNickName.java
+++ L2J_Server_BETA/java/com/l2jserver/gameserver/network/clientpackets/RequestGiveNickName.java
@@ -22,12 +22,7 @@ import com.l2jserver.gameserver.model.L2Clan;
 import com.l2jserver.gameserver.model.L2ClanMember;
 import com.l2jserver.gameserver.model.actor.instance.L2PcInstance;
 import com.l2jserver.gameserver.network.SystemMessageId;
-import com.l2jserver.gameserver.network.serverpackets.SystemMessage;
 
-/**
- * This class ...
- * @version $Revision: 1.3.2.1.2.4 $ $Date: 2005/03/27 15:29:30 $
- */
 public class RequestGiveNickName extends L2GameClientPacket
 {
 	private static final String _C__0B_REQUESTGIVENICKNAME = "[C] 0B RequestGiveNickName";
@@ -52,21 +47,24 @@ public class RequestGiveNickName extends L2GameClientPacket
 		}
 		
 		// Noblesse can bestow a title to themselves
-		if (activeChar.isNoble() && _target.matches(activeChar.getName()))
+		if (activeChar.isNoble() && _target.equalsIgnoreCase(activeChar.getName()))
 		{
 			activeChar.setTitle(_title);
-			SystemMessage sm = SystemMessage.getSystemMessage(SystemMessageId.TITLE_CHANGED);
-			activeChar.sendPacket(sm);
+			activeChar.sendPacket(SystemMessageId.TITLE_CHANGED);
 			activeChar.broadcastTitleInfo();
 		}
-		// Can the player change/give a title?
-		else if (activeChar.hasClanPrivilege(L2Clan.CP_CL_GIVE_TITLE))
+		else
 		{
+			// Can the player change/give a title?
+			if (!activeChar.hasClanPrivilege(L2Clan.CP_CL_GIVE_TITLE))
+			{
+				activeChar.sendPacket(SystemMessageId.YOU_ARE_NOT_AUTHORIZED_TO_DO_THAT);
+				return;
+			}
+			
 			if (activeChar.getClan().getLevel() < 3)
 			{
-				SystemMessage sm = SystemMessage.getSystemMessage(SystemMessageId.CLAN_LVL_3_NEEDED_TO_ENDOWE_TITLE);
-				activeChar.sendPacket(sm);
-				sm = null;
+				activeChar.sendPacket(SystemMessageId.CLAN_LVL_3_NEEDED_TO_ENDOWE_TITLE);
 				return;
 			}
 			
@@ -83,13 +81,12 @@ public class RequestGiveNickName extends L2GameClientPacket
 				}
 				else
 				{
-					// TODO retail messages
-					activeChar.sendMessage("Target needs to be online to get a title");
+					activeChar.sendPacket(SystemMessageId.TARGET_IS_NOT_FOUND_IN_THE_GAME);
 				}
 			}
 			else
 			{
-				activeChar.sendMessage("Target does not belong to your clan");
+				activeChar.sendPacket(SystemMessageId.TARGET_MUST_BE_IN_CLAN);
 			}
 		}
 	}
