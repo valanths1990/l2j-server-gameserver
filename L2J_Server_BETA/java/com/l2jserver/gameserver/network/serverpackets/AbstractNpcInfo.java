6b8fe69473e93d486c94b66d163544c44af5bf41
St3eT <St3eT@users.noreply.github.com>
2014-02-19 21:38:45 +0000

    BETA: Fixed display aggressive monsters. 	* Reported by: laxsur 	* Reviewed by: Nos, !UnAfraid, malyelfik

--- L2J_Server_BETA/java/com/l2jserver/gameserver/network/serverpackets/AbstractNpcInfo.java
+++ L2J_Server_BETA/java/com/l2jserver/gameserver/network/serverpackets/AbstractNpcInfo.java
@@ -114,7 +114,7 @@ public abstract class AbstractNpcInfo extends L2GameServerPacket
 			
 			if (Config.SHOW_NPC_LVL && (_npc instanceof L2MonsterInstance))
 			{
-				String t = "Lv " + cha.getLevel() + (cha.getAggroRange() > 0 ? "*" : "");
+				String t = "Lv " + cha.getLevel() + (cha.isAggressive() ? "*" : "");
 				if (_title != null)
 				{
 					t += " " + _title;
