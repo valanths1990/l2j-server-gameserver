bb2e94cc84e67798c03f59768079aaea9063c58b
Rumen Nikiforov <unafraid89@gmail.com>
2014-10-01 23:35:09 +0000

    BETA: Adding small abstract class to simplify SystemMessage and ConfirmDlg and decrease the duplicated code also implementing full support of ConfirmDlg packet. 	* Tested by: Micr0

--- L2J_Server_BETA/java/com/l2jserver/gameserver/network/serverpackets/ConfirmDlg.java
+++ L2J_Server_BETA/java/com/l2jserver/gameserver/network/serverpackets/ConfirmDlg.java
@@ -18,181 +18,31 @@
  */
 package com.l2jserver.gameserver.network.serverpackets;
 
-import javolution.util.FastList;
-
-import com.l2jserver.gameserver.model.actor.L2Character;
-import com.l2jserver.gameserver.model.actor.L2Npc;
-import com.l2jserver.gameserver.model.actor.L2Summon;
-import com.l2jserver.gameserver.model.actor.instance.L2PcInstance;
-import com.l2jserver.gameserver.model.actor.templates.L2NpcTemplate;
-import com.l2jserver.gameserver.model.items.L2Item;
-import com.l2jserver.gameserver.model.items.instance.L2ItemInstance;
-import com.l2jserver.gameserver.model.skills.BuffInfo;
-import com.l2jserver.gameserver.model.skills.Skill;
 import com.l2jserver.gameserver.network.SystemMessageId;
 
 /**
  * ConfirmDlg server packet implementation.
- * @author kombat
+ * @author kombat, UnAfraid
  */
-public class ConfirmDlg extends L2GameServerPacket
+public class ConfirmDlg extends AbstractMessagePacket<ConfirmDlg>
 {
-	private final int _messageId;
+	private int _time;
+	private int _requesterId;
 	
-	private int _skillLvL = 1;
-	
-	private static final int TYPE_ZONE_NAME = 7;
-	private static final int TYPE_SKILL_NAME = 4;
-	private static final int TYPE_ITEM_NAME = 3;
-	private static final int TYPE_NPC_NAME = 2;
-	private static final int TYPE_NUMBER = 1;
-	private static final int TYPE_TEXT = 0;
-	
-	private final FastList<CnfDlgData> _info = new FastList<>();
-	
-	private int _time = 0;
-	private int _requesterId = 0;
-	
-	private static class CnfDlgData
+	public ConfirmDlg(SystemMessageId smId)
 	{
-		protected final int type;
-		protected final Object value;
-		
-		protected CnfDlgData(int t, Object val)
-		{
-			type = t;
-			value = val;
-		}
+		super(smId);
 	}
 	
-	public ConfirmDlg(int messageId)
+	public ConfirmDlg(int id)
 	{
-		_messageId = messageId;
-	}
-	
-	public ConfirmDlg(SystemMessageId messageId)
-	{
-		this(messageId.getId());
+		this(SystemMessageId.getSystemMessageId(id));
 	}
 	
 	public ConfirmDlg(String text)
 	{
 		this(SystemMessageId.S1);
 		addString(text);
-	}
-	
-	public ConfirmDlg addString(String text)
-	{
-		_info.add(new CnfDlgData(TYPE_TEXT, text));
-		return this;
-	}
-	
-	public ConfirmDlg addNumber(int number)
-	{
-		_info.add(new CnfDlgData(TYPE_NUMBER, number));
-		return this;
-	}
-	
-	public ConfirmDlg addCharName(L2Character cha)
-	{
-		if (cha.isNpc())
-		{
-			return addNpcName((L2Npc) cha);
-		}
-		if (cha.isPlayer())
-		{
-			return addPcName(cha.getActingPlayer());
-		}
-		if (cha.isSummon())
-		{
-			return addNpcName((L2Summon) cha);
-		}
-		return addString(cha.getName());
-	}
-	
-	public ConfirmDlg addPcName(L2PcInstance pc)
-	{
-		return addString(pc.getAppearance().getVisibleName());
-	}
-	
-	public ConfirmDlg addNpcName(L2Npc npc)
-	{
-		return addNpcName(npc.getTemplate());
-	}
-	
-	public ConfirmDlg addNpcName(L2Summon npc)
-	{
-		return addNpcName(npc.getId());
-	}
-	
-	public ConfirmDlg addNpcName(L2NpcTemplate tpl)
-	{
-		if (tpl.isUsingServerSideName())
-		{
-			return addString(tpl.getName());
-		}
-		return addNpcName(tpl.getId());
-	}
-	
-	public ConfirmDlg addNpcName(int id)
-	{
-		_info.add(new CnfDlgData(TYPE_NPC_NAME, id));
-		return this;
-	}
-	
-	public ConfirmDlg addItemName(L2ItemInstance item)
-	{
-		return addItemName(item.getItem().getId());
-	}
-	
-	public ConfirmDlg addItemName(L2Item item)
-	{
-		// TODO: template id for items
-		return addItemName(item.getId());
-	}
-	
-	public ConfirmDlg addItemName(int id)
-	{
-		_info.add(new CnfDlgData(TYPE_ITEM_NAME, id));
-		return this;
-	}
-	
-	public ConfirmDlg addZoneName(int x, int y, int z)
-	{
-		Integer[] coord =
-		{
-			x,
-			y,
-			z
-		};
-		_info.add(new CnfDlgData(TYPE_ZONE_NAME, coord));
-		return this;
-	}
-	
-	public ConfirmDlg addSkillName(BuffInfo info)
-	{
-		return addSkillName(info.getSkill());
-	}
-	
-	public ConfirmDlg addSkillName(Skill skill)
-	{
-		if (skill.getId() != skill.getDisplayId())
-		{
-			return addString(skill.getName());
-		}
-		return addSkillName(skill.getId(), skill.getLevel());
-	}
-	
-	public ConfirmDlg addSkillName(int id)
-	{
-		return addSkillName(id, 1);
-	}
-	
-	public ConfirmDlg addSkillName(int id, int lvl)
-	{
-		_info.add(new CnfDlgData(TYPE_SKILL_NAME, id));
-		_skillLvL = lvl;
-		return this;
 	}
 	
 	public ConfirmDlg addTime(int time)
@@ -210,54 +60,9 @@ public class ConfirmDlg extends L2GameServerPacket
 	@Override
 	protected final void writeImpl()
 	{
-		writeC(0xf3);
-		writeD(_messageId);
-		
-		if (_info.isEmpty())
-		{
-			writeD(0x00);
-			writeD(_time);
-			writeD(_requesterId);
-		}
-		else
-		{
-			writeD(_info.size());
-			
-			// TODO: Verify this i bet its same like SystemMessage
-			for (CnfDlgData data : _info)
-			{
-				writeD(data.type);
-				
-				switch (data.type)
-				{
-					case TYPE_TEXT:
-						writeS((String) data.value);
-						break;
-					case TYPE_NUMBER:
-					case TYPE_NPC_NAME:
-					case TYPE_ITEM_NAME:
-						writeD((Integer) data.value);
-						break;
-					case TYPE_SKILL_NAME:
-						writeD((Integer) data.value); // Skill Id
-						writeD(_skillLvL); // Skill lvl
-						break;
-					case TYPE_ZONE_NAME:
-						Integer[] array = (Integer[]) data.value;
-						writeD(array[0]);
-						writeD(array[1]);
-						writeD(array[2]);
-						break;
-				}
-			}
-			if (_time != 0)
-			{
-				writeD(_time);
-			}
-			if (_requesterId != 0)
-			{
-				writeD(_requesterId);
-			}
-		}
+		writeC(0xF3);
+		writeMe();
+		writeD(_time);
+		writeD(_requesterId);
 	}
 }
