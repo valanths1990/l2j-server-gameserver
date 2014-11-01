LucusAngel/L2J_Server_BETA

Refactored by jurchicks' request
rate by ID doesn't stack with standard rates anymore, it overrides standard retes instead (as it did prior this rework)

fixed few small bugs

java/com/l2jserver/gameserver/model/drops/GroupedCorpseDropItem.java
@@ -19,7 +19,6 @@
 package com.l2jserver.gameserver.model.drops;
 
 import com.l2jserver.Config;
-import com.l2jserver.gameserver.model.actor.L2Character;
 
 /**
  * @author Nos
@@ -36,11 +35,11 @@ public class GroupedCorpseDropItem extends GroupedGeneralDropItem
 	
 	/*
 	 * (non-Javadoc)
-	 * @see com.l2jserver.gameserver.model.drops.GroupedGeneralDropItem#getChance(com.l2jserver.gameserver.model.actor.L2Character, com.l2jserver.gameserver.model.actor.L2Character)
+	 * @see com.l2jserver.gameserver.model.drops.GroupedGeneralDropItem#getDefaultChanceMultiplier()
 	 */
 	@Override
-	public double getChance(L2Character victim, L2Character killer)
+	protected double getDefaultChanceMultiplier()
 	{
-		return super.getChance(victim, killer) * (isHerbOnly() ? Config.RATE_HERB_DROP_AMOUNT_MULTIPLIER : Config.RATE_CORPSE_DROP_CHANCE_MULTIPLIER);
+		return Config.RATE_CORPSE_DROP_CHANCE_MULTIPLIER;
 	}
 }

java/com/l2jserver/gameserver/model/drops/GroupedDeathDropItem.java
@@ -19,8 +19,6 @@
 package com.l2jserver.gameserver.model.drops;
 
 import com.l2jserver.Config;
-import com.l2jserver.gameserver.model.actor.L2Character;
-import com.l2jserver.gameserver.model.actor.instance.L2RaidBossInstance;
 
 /**
  * @author Nos
@@ -37,11 +35,11 @@ public class GroupedDeathDropItem extends GroupedGeneralDropItem
 	
 	/*
 	 * (non-Javadoc)
-	 * @see com.l2jserver.gameserver.model.drops.GroupedGeneralDropItem#getChance(com.l2jserver.gameserver.model.actor.L2Character, com.l2jserver.gameserver.model.actor.L2Character)
+	 * @see com.l2jserver.gameserver.model.drops.GroupedGeneralDropItem#getDefaultChanceMultiplier()
 	 */
 	@Override
-	public double getChance(L2Character victim, L2Character killer)
+	protected double getDefaultChanceMultiplier()
 	{
-		return super.getChance(victim, killer) * (isHerbOnly() ? Config.RATE_HERB_DROP_AMOUNT_MULTIPLIER : (victim instanceof L2RaidBossInstance ? Config.RATE_RAID_DROP_CHANCE_MULTIPLIER : Config.RATE_DEATH_DROP_CHANCE_MULTIPLIER));
+		return Config.RATE_DEATH_DROP_CHANCE_MULTIPLIER;
 	}
 }

valanths1990/l2j-server-gameserver

L2J_Server/java/com/l2jserver/gameserver/model/drops/GroupedCorpseDropItem.java
@@ -19,8 +19,6 @@
 package com.l2jserver.gameserver.model.drops;
 
 import com.l2jserver.Config;
-import com.l2jserver.gameserver.model.actor.L2Character;
-import com.l2jserver.gameserver.model.actor.instance.L2RaidBossInstance;
 
 /**
  * @author Nos
@@ -37,11 +35,11 @@ public class GroupedDeathDropItem extends GroupedGeneralDropItem
 	
 	/*
 	 * (non-Javadoc)
-	 * @see com.l2jserver.gameserver.model.drops.GroupedGeneralDropItem#getChance(com.l2jserver.gameserver.model.actor.L2Character, com.l2jserver.gameserver.model.actor.L2Character)
+	 * @see com.l2jserver.gameserver.model.drops.GroupedGeneralDropItem#getDefaultChanceMultiplier()
 	 */
 	@Override
-	public double getChance(L2Character victim, L2Character killer)
+	protected double getDefaultChanceMultiplier()
 	{
-		return super.getChance(victim, killer) * (isHerbOnly() ? Config.RATE_HERB_DROP_AMOUNT_MULTIPLIER : (victim instanceof L2RaidBossInstance ? Config.RATE_RAID_DROP_CHANCE_MULTIPLIER : Config.RATE_DEATH_DROP_CHANCE_MULTIPLIER));
+		return Config.RATE_DEATH_DROP_CHANCE_MULTIPLIER;
 	}
 }

L2J_Server/java/com/l2jserver/gameserver/model/drops/GroupedDeathDropItem.java
@@ -19,7 +19,6 @@
 package com.l2jserver.gameserver.model.drops;
 
 import com.l2jserver.Config;
-import com.l2jserver.gameserver.model.actor.L2Character;
 
 /**
  * @author Nos
@@ -36,11 +35,11 @@ public class GroupedCorpseDropItem extends GroupedGeneralDropItem
 	
 	/*
 	 * (non-Javadoc)
-	 * @see com.l2jserver.gameserver.model.drops.GroupedGeneralDropItem#getChance(com.l2jserver.gameserver.model.actor.L2Character, com.l2jserver.gameserver.model.actor.L2Character)
+	 * @see com.l2jserver.gameserver.model.drops.GroupedGeneralDropItem#getDefaultChanceMultiplier()
 	 */
 	@Override
-	public double getChance(L2Character victim, L2Character killer)
+	protected double getDefaultChanceMultiplier()
 	{
-		return super.getChance(victim, killer) * (isHerbOnly() ? Config.RATE_HERB_DROP_AMOUNT_MULTIPLIER : Config.RATE_CORPSE_DROP_CHANCE_MULTIPLIER);
+		return Config.RATE_CORPSE_DROP_CHANCE_MULTIPLIER;
 	}
 }