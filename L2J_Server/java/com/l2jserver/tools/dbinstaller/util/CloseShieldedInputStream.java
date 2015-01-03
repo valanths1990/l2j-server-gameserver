d8be37d2a2f73dcdfe1b5907bf2d496280eaf557
Zoey76 <zoey_76@msn.com>
2015-01-03 01:49:41 -0200

    Removing Community Server database installer Moved CloseShieldedInputStream to tool/dbinstaller/util

--- L2J_Server/java/com/l2jserver/tools/dbinstaller/util/CloseShieldedInputStream.java
+++ L2J_Server/java/com/l2jserver/tools/dbinstaller/util/CloseShieldedInputStream.java
@@ -16,7 +16,7 @@
  * You should have received a copy of the GNU General Public License
  * along with this program. If not, see <http://www.gnu.org/licenses/>.
  */
-package com.l2jserver.util;
+package com.l2jserver.tools.dbinstaller.util;
 
 import java.io.IOException;
 import java.io.InputStream;
