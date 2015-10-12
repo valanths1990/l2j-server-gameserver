3d628149c85a811f4211236f081832fde006be55
Zoey76 <zoey_76@msn.com>
2015-10-12 17:26:53 -0200

    Improvement to attack delay
    Thanks @Nik--

--- src/main/java/com/l2jserver/gameserver/model/actor/L2Character.java
+++ src/main/java/com/l2jserver/gameserver/model/actor/L2Character.java
@@ -1202,7 +1202,7 @@ public abstract class L2Character extends L2Object implements ISkillsHolder, IDe
 			}
 			
 			// Notify AI with EVT_READY_TO_ACT
-			ThreadPoolManager.getInstance().scheduleAi(new NotifyAITask(this, CtrlEvent.EVT_READY_TO_ACT), (timeAtk + reuse) / 2);
+			ThreadPoolManager.getInstance().scheduleAi(new NotifyAITask(this, CtrlEvent.EVT_READY_TO_ACT), timeAtk);
 		}
 		finally
 		{
