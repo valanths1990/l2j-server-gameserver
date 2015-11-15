914bb3f6f3a6372c1451a865edab57b735df118c
Zealar <zealar@mail.bg>
2015-11-15 11:01:07 +0200

    Fix #135

--- src/main/java/com/l2jserver/gameserver/network/serverpackets/UserInfo.java
+++ src/main/java/com/l2jserver/gameserver/network/serverpackets/UserInfo.java
@@ -169,10 +169,6 @@ public final class UserInfo extends L2GameServerPacket
 		writeD(_activeChar.isGM() ? 1 : 0); // builder level
 		
 		String title = _activeChar.getTitle();
-		if (_activeChar.isInvisible())
-		{
-			title = "Invisible";
-		}
 		if (_activeChar.getPoly().isMorphed())
 		{
 			final L2NpcTemplate polyObj = NpcData.getInstance().getTemplate(_activeChar.getPoly().getPolyId());
