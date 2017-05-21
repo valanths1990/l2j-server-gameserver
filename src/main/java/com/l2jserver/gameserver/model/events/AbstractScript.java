ec8298f00bf100319135214aeb405c4221fa5bda
MichaelYu <790710267@qq.com>
2017-05-21 15:39:33 +0800

    Fix sometimes players can teleport to other party's instance
    Fix sometimes party teleport to a empty room

--- src/main/java/com/l2jserver/gameserver/model/events/AbstractScript.java
+++ src/main/java/com/l2jserver/gameserver/model/events/AbstractScript.java
@@ -3185,9 +3185,7 @@ public abstract class AbstractScript implements INamable
 	 */
 	public void teleportPlayer(L2PcInstance player, Location loc, int instanceId, boolean allowRandomOffset)
 	{
-		Location copy = new Location(loc.getX(), loc.getY(), loc.getZ());
-		copy.setInstanceId(instanceId);
-		player.teleToLocation(copy, allowRandomOffset);
+		player.teleToLocation(loc, instanceId, allowRandomOffset ? Config.MAX_OFFSET_ON_TELEPORT : 0);
 	}
 	
 	/**
